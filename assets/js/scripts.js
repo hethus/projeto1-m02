4e327e1ec6c76bab329636b49d75c3bd65a79e25
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-04-16 15:02:45 -0300

    projeto base finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 const alterar = document.querySelectorAll(".role");
+const botao = document.querySelector("#alternative");
 
 function handleChange() {
   let numero = Math.floor(Math.random() * 5) + 1;
-  console.log(alterar);
+    botao.innerText = "Mudar a Role!";
 
   if (numero === 1) {
-    //alterar img
     alterar[0].setAttribute("src", "./assets/img/top.png");
     alterar[1].innerHTML =
       "<h3>Top laner</h3><br><h3>É a Lane localizada na extremidade superior do mapa. A Top Lane geralmente abriga geralmente campeões Lutadores ou Tanques, que absorvem grandes quantias de dano e desferem uma quantidade mediana de dano em troca. Normalmente, a rota está livre de preocupações adicionais, o que a torna a melhor rota para iniciantes.</h3>";
@@ -18,7 +18,7 @@ function handleChange() {
   } else if (numero === 3) {
     alterar[0].setAttribute("src", "./assets/img/mid.png");
     alterar[1].innerHTML =
-      "<h3>Mid Lane</h3><br><h3>É a Lane localizada no centro do mapa (Para Ambos os Times). A Mid Lane abriga campeões variados, em sua maioria Magos com alto alcance, que conseguem jogar de forma segura e eficaz, e Assassinos com alta mobilidade, que conseguem chegar perto do alvo rapidamente e desferir uma grande quantia de dano em pouco tempo. Os dois exemplos citados tem alto dano, considerados como dano Explosivo (muito dano em pouco tempo). A rota é mediana em termos de dificuldade, e apresenta as responsabilidades de avisar sobre inimigos avistandos indo para certo lugar (MIA), além de também ser uma rota na qual você pode sair e vaguear (GANKAR) para outra rota, normalmente para ajudar ou garantir um avanço.</h3>";
+      "<h3>Mid Lane</h3><br><h3>É a Lane localizada no centro do mapa (Para Ambos os Times). A Mid Lane abriga campeões variados, em sua maioria Magos com alto alcance, que conseguem jogar de forma segura e eficaz, e Assassinos com alta mobilidade, que conseguem chegar perto do alvo rapidamente e desferir uma grande quantia de dano em pouco tempo. A rota é mediana em termos de dificuldade, e apresenta as responsabilidades de avisar sobre inimigos avistados indo para certo lugar (MIA), além de também ser uma rota na qual você pode sair e vaguear (GANKAR) para outra rota, normalmente para ajudar ou garantir um avanço.</h3>";
     alterar[2].setAttribute("src", "https://www.youtube.com/embed/EBzzjHXKXYE");
   } else if (numero === 4) {
     alterar[0].setAttribute("src", "./assets/img/bot.png");
@@ -31,6 +31,4 @@ function handleChange() {
       "<h3>Suporte</h3><br><h3>São voltados, normalmente, a Utilidade (Cura, Remoção de Controles de Grupo, etc.) ou ao Controle de Grupo (Enraízamento, Atordoamento, etc.), o que ajuda o time em batalhas. Ficam a maior parte do tempo junto do AD Carry na rota inferior (BOT LANE) localizada na extremidade inferior do mapa. Eles costumam não necessitar de itens para si mesmos como os outros do time, ou seja, não farmam ou matam. O ouro de um suporte é proveniente, em sua maioria, das Assistências.</h3>";
     alterar[2].setAttribute("src", "https://www.youtube.com/embed/LOpZnGIXsVk");
   }
-
-  console.log(alterar);
 }
